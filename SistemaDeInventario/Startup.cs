78bd43938300c7c114e385bc3c3b7d9218f51c9a
Diego Villacis <Diego Villacis>
2016-05-02 22:56:27 -0500

    First commit

---
+++ SistemaDeInventario/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SistemaDeInventario.Startup))]
+namespace SistemaDeInventario
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
